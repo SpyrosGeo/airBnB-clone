69ba13ef55f7b2b529926948f28975b7b80a6d8e
SpyrosGeo <xarodasimba@gmail.com>
2021-05-08 19:25:54 +0300

    added basic nextjs code

--- schemas/person.js
+++ schemas/person.js
@@ -5,28 +5,34 @@ export default {
     fields: [
         {
             name: 'name',
-            title:'Name',
-            type:'string',
-            description:'Please use " Firstname Lastname format "'
+            title: 'Name',
+            type: 'string',
+            description: 'Please use " Firstname Lastname format "'
         },
         {
-            name:'slug',
-            title:'Slug',
-            type:'slug',
-            options:{
-                source:'name',
-                maxLength:100
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 100
             }
         },
         {
-            name:'id',
-            title:'Id',
-            type:'number',
+            name: 'id',
+            title: 'Id',
+            type: 'number',
         },
         {
-            name:'image',
-            title:'Image',
-            type:'image',
+            name: 'image',
+            title: 'Image',
+            type: 'image',
         },
-    ]
+    ],
+    //optional to set what we see on CMS
+    preview: {
+        select: {
+            title: 'name', media: 'image'
+        }
+    }
 }
